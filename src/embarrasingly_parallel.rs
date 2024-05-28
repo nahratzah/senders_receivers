5e0e4775c4faa98fb5e21b96685bee75a2ed2898
Ariane van der Steldt <ariane@stack.nl>
2024-05-28 18:29:04 +0100

    add examples for using embarrasingly-parallel module

--- src/embarrasingly_parallel.rs
+++ src/embarrasingly_parallel.rs
@@ -4,9 +4,52 @@
 //! To use a thread-pool, instantiate a [ThreadPool].
 //! Submitting tasks on the [ThreadPool] will schedule them to run on an arbitrary thread.
 //!
+//! ```
+//! use senders_receivers::embarrasingly_parallel::{ThreadPool, ThreadLocalPool};
+//! use senders_receivers::{new_error, start_detached, Scheduler, Then};
+//! use std::net::{TcpListener, TcpStream};
+//! use std::io::Write;
+//!
+//! let pool = ThreadPool::new(4); // Create a pool with 4 worker threads.
+//! let listener = TcpListener::bind("[::1]:0").unwrap();
+//! listener.set_nonblocking(true).unwrap(); // stops the example from hanging
+//!
+//! for stream in listener.incoming() {
+//!     match stream {
+//!         Ok(stream) => {
+//!             start_detached(
+//!                 pool.schedule_value((stream,))
+//!                 | Then::from(
+//!                     |(mut stream,): (TcpStream,)| {
+//!                         stream.write_all(b"hello world\n").map_err(new_error)
+//!                     }))
+//!         },
+//!         Err(_) => break,
+//!     }
+//! }
+//! ```
+//!
 //! ## Manual Mode
 //! To manually operate the scheduler, start by creating a [ThreadLocalPool] and associated [Worker].
 //! Tasks will be run when you call [Worker::run] or [Worker::run_one].
+//!
+//! ```
+//! use senders_receivers::embarrasingly_parallel::Worker;
+//! use senders_receivers::{start_detached, Scheduler, Then};
+//! use std::thread;
+//!
+//! fn print_number((i,): (i32,)) {
+//!     println!("task {} running in {:?}\n", i, thread::current().id());
+//! }
+//!
+//! let (pool, worker) = Worker::new();
+//! for i in 0..10 {
+//!     start_detached(pool.schedule_value((i,)) | Then::from(print_number));
+//! }
+//! drop(pool); // Without this, the worker.run() function will never complete.
+//!
+//! worker.run(); // Run all the tasks.
+//! ```
 
 mod cross_thread_pool;
 mod pool;
