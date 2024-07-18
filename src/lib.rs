deaff84e2f070205c5a38c97a36ae9d00f9f147c
Ariane van der Steldt <ariane@stack.nl>
2024-07-18 16:12:17 +0100

    update documentation, update cargo manifest

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,11 @@
 
 //! This is an implementation (for certain value of implementation) of C++ senders/receivers.
 //!
-//! ## A Tiny Example
+//! # Note
+//! This code is very much a work-in-progress, and large parts are still missing.
+//! The API is also still in flux.
+//!
+//! # A Tiny Example
 //! ```
 //! use senders_receivers::*;
 //!
@@ -19,7 +23,7 @@
 //!
 //! None of the steps are run, until `sync_wait` is invoked.
 //!
-//! ## Senders? Receivers?
+//! # Senders? Receivers?
 //! The main concept of this code comes from having both senders and receivers.
 //! A sender is a type that produces a value signal (or an error signal, or a done signal).
 //! A receiver is a type that accepts this signal.
@@ -29,7 +33,7 @@
 //!
 //! If you are using the library, you will not spot any receivers, unless you're implementing your own extensions.
 //!
-//! ## Signals
+//! # Signals
 //! Each sender, produces either
 //! a `value signal`,
 //! an `error signal`,
@@ -40,7 +44,7 @@
 //! An `error signal` indicates that the sender failed, and produced an [Error].  
 //! A `done signal` indicates that the sender canceled its work (producing neither a value, nor an error).
 //!
-//! ## Schedulers
+//! # Schedulers
 //! Each operation will run on a [Scheduler].
 //! (Sometimes more than one, for example if you [Transfer] to a different scheduler.)
 //!
@@ -51,8 +55,8 @@
 //! - [ImmediateScheduler] runs every task immediately. This is more-or-less the default scheduler.
 //! - [ThreadPool](threadpool::ThreadPool) runs tasks using a threadpool.
 //!
-//! ## Comparison With C++
-//! An attempt is made to remain close the the usability of the C++ style,
+//! # Comparison With C++
+//! An attempt is made to remain close the the usability of [its C++ counterpart](https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2021/p2300r1.html),
 //! but sacrifices had to be made.
 //!
 //! - The `Error` type is type-erased/dynamic.  
@@ -61,8 +65,10 @@
 //!   For a non-tuple type, we would require variadic type arguments.
 //! - The `Value` is not a variant.  
 //!   For a variant-type, we would require a variant-type with variadic type arguments.
+//!   Furthermore, rust closures bind their type on first use, dispatching to overloads would be difficult.
 //! - Scheduler-based specializations are omitted.  
 //!   I think for this, we would require specializations.
+//!   (Although the [io::write](crate::io::Write) implementation does permit specialization, using an opt-in for a default implementation.)
 //! - Connect can no longer fail.  
 //!   In C++, the `connect` call can fail, and this will result in an error being propagated.
 //!   This meant that when the `connect` call inside a `let_value` step fails,
@@ -75,11 +81,13 @@
 //!   And also, rust does not allow in-place construction (which is required for nested operation states; the C++ design requires in-place construction).
 //!   Instead we wrap subsequent operations into a receiver, during the connect call.
 //!   This has the small disadvantage we cannot do any preparations during the [OperationState::start] step.
+//! - [LetValue] arguments are passed by reference (similar to C++) but cannot be taken ownership of.  
+//!   Instead, the arguments are retained and combined with the values from the sender-chain returned by the LetValue function.
 //!
-//! Some sacrifices stem from me disagreeing with the C++ design.
+//! Some sacrifices stem from me disagreeing with the C++ design:
 //! I liked the promise from the design, that scheduler changes are explicit only.
 //! But in practice, it was very hard to use, and my code, once async, always needed to grab the receiver-scheduler
-//! (because it was too common for the sender not to have a scheduler).
+//! (because it was too common for the sender not to have a (known) scheduler).
 //!
 //! - The `done` and `error` channels no longer have an associated scheduler.  
 //!   There is no way to properly guarantee which scheduler an error is propagated upon.
@@ -92,9 +100,9 @@
 //! - Schedulers have a LocalScheduler type.  
 //!   For most schedulers, that will be the scheduler itself.
 //!   This allows for schedulers to declare follow-up code to run on a different scheduler.
-//!   For example, an embarrasingly-parallel scheduler could schedule on any thread the first time,
-//!   and then propagate a scheduler that'll always use the same thread for subsequent scheduling.
-//! - Error/done recovery operations must complete with the same scheduler-type and value-type, as what would be sent during the happy path.  
+//!   For example, [the embarrasingly-parallel scheduler](crate::embarrasingly_parallel::ThreadPool) schedules on any thread the first time,
+//!   and then propagates [a scheduler](crate::embarrasingly_parallel::ThreadLocalPool) that'll always use the same thread for subsequent scheduling.
+//! - Error/done recovery operations must complete with the same scheduler-type and value-type, as what would be sent during the value path.  
 //!   This is a consequence of us limiting things to a single type, and sending the scheduler along in the value-signal.
 //!   (Note that the error and done signals don't propagate a scheduler alongside; this would be impossible without making [LetValue] a lot more cumbersome to use.)
 //!
