c6828d8f23ae8d9402e3f6085fbefe5fe9959802
Ariane van der Steldt <ariane@stack.nl>
2024-07-21 11:40:21 +0100

    Mention that io is a partial implementation only, for now

--- src/io.rs
+++ src/io.rs
@@ -2,6 +2,10 @@
 //!
 //! It decorates read/write/etc types, such that they play nice with [senders/receivers](crate::traits::TypedSender).
 //! For example, it'll add a `fd.write(Scheduler, buffer)` (yielding a [TypedSender](crate::traits::TypedSender) to the [File](std::fs::File) type.
+//!
+//! # Unfinished
+//! This is unfinished, I'm still working on this.
+//! I implemented a small bit, so I could verify [LetValue](crate::let_value::LetValue) works as I want it to.
 
 mod default;
 mod write;
